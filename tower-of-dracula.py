6549f218e4ab38f53f84c39ba5c7fcb57e3cb945
Nicholas Farley <ohcrap524@gmail.com>
2013-12-02 16:00:09 -0500

    finalizing bat enemy

--- tower-of-dracula.py
+++ tower-of-dracula.py
@@ -282,14 +282,15 @@ class Bat(Actor):
         self.image = self.image1
 
         self.hitboxoffset = 0
-        self.rect = Rect(xpos+self.hitboxoffset-10/2, ypos-10/2, 10, 10)
-        self.swoop = 100
+        self.rect = Rect(xpos+self.hitboxoffset-30/2, ypos-30/2, 30, 30)
+        self.swoop = 180
         self.swoop_frame = self.swoop
-        self.swoop_velocity = 5
-        self.swoop_decay = .2
+        self.swoop_velocity = 5.0
+        self.swoop_decay = .1
         self.velocity = 0
         self.xvector = 0
         self.yvector = 0
+        self.frame = FPS
 
     def update(self, world):
         '''enemy AI processing'''
@@ -297,7 +298,7 @@ class Bat(Actor):
         self.movy = 0
         self.image = self.image1
 
-        if self.velocity > 0:
+        if self.velocity > self.swoop_decay:
             self.velocity -= self.swoop_decay
 
         if self.swoop_frame > 0:
@@ -306,18 +307,39 @@ class Bat(Actor):
             self.swoop_frame = self.swoop
             self.velocity = self.swoop_velocity
             self.yvector = world.simon.rect.y
-            self.xvector = world.simon.rect.x
 
-        if self.xvector < self.rect.x:
+            if world.simon.rect.x < self.rect.x:
+                self.xvector = -1
+            else:
+                self.xvector = 1
+
+        if self.xvector < 0:
             self.movx -= self.velocity
-        elif self.xvector > self.rect.x:
+        elif self.xvector > 0:
             self.movx += self.velocity
-        if self.yvector < self.rect.y:
+
+        if self.yvector <= self.rect.y:
+            self.yvector -= 5
             self.movy -= self.velocity
         elif self.yvector > self.rect.y: 
             self.movy += self.velocity
 
+        self.rect.x += self.movx
+        self.rect.y += self.movy
 
+
+        if self.frame > 0:
+            self.frame -= 1
+        else: 
+            self.frame = FPS
+
+        f = self.frame / 10
+        if f is 1 or f is 4:
+            self.image = self.image1
+        elif f is 2 or f is 5:
+            self.image = self.image2
+        elif f is 3:
+            self.image = self.image3
 
         if self.movx < 0:
             self.direction = "Left"
@@ -326,14 +348,6 @@ class Bat(Actor):
 
         if self.direction is "Right":
             self.image = pygame.transform.flip(self.image, True, False)
-
-
-        self.rect.x += self.movx
-        self.rect.y += self.movy
-
-        
-
-
 
 class Zombie(Actor):
     '''Class that represents zombies in the game world'''
