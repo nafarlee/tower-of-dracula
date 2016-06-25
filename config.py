4370cedb341be343a3822b9efcabc9831d225f82
Nicholas Farley <ohcrap524@gmail.com>
2016-06-25 17:48:23 -0400

    Add get function to config module

--- config.py
+++ config.py
@@ -14,3 +14,8 @@ def validate(config_dict):
     assert isinstance(multiplayer, bool), "invalid multiplayer configuration"
     port = config_dict["port"]
     assert port > 0 and port < 65537, "invalid port configuration"
+
+def get():
+    config_dict = load()
+    validate(config_dict)
+    return config_dict
