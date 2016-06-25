745cedace006374237a780a4e519b2c863c7bb12
Nicholas Farley <ohcrap524@gmail.com>
2016-06-25 17:47:49 -0400

    Add validate function to config module

--- config.py
+++ config.py
@@ -6,3 +6,11 @@ def load():
     file_contents = file_object.read()
 
     return json.loads(file_contents)
+
+def validate(config_dict):
+    player = config_dict["player"]
+    assert player == "Simon" or player == "Dracula", "invalid player configuration"
+    multiplayer = config_dict["multiplayer"]
+    assert isinstance(multiplayer, bool), "invalid multiplayer configuration"
+    port = config_dict["port"]
+    assert port > 0 and port < 65537, "invalid port configuration"
