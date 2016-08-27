1015a30fea9c395d1bb5ec2aefdf2e5f14b935dc
Nicholas Farley <ohcrap524@gmail.com>
2016-08-27 16:49:22 -0400

    Extract blit_large_label fn to separate module

---
+++ render.py
@@ -0,0 +1,12 @@
+import pygame
+
+def blit_large_label(screen, message):
+    print screen
+    font_size = 100
+    width, height = screen.get_size()
+    labelx = width/3 - font_size
+    labely = height/2
+    font = pygame.font.SysFont(None, font_size)
+    label = font.render(message, 1, (255, 255, 255))
+
+    screen.blit(label, (labelx, labely))
