f9ffb4fff715c672b35b152219689bd95cf5808c
Nicholas Farley <ohcrap524@gmail.com>
2016-08-27 14:43:07 -0400

    Extract network related methods to network.py module

---
+++ network.py
@@ -0,0 +1,38 @@
+import cPickle as pickle
+
+def send_world_report(world, socket):
+    """send the pertinent world information to the second player"""
+    world_report = {
+        "Simon": world.simon.rect,
+        "Health": world.simon.health,
+        "MP": world.mp,
+        "Time": world.time,
+        "Enemies": [],
+        "Winner": world.winner
+    }
+
+    for enemy in world.enemies:
+        enemy_summary = (enemy.__name__, enemy.rect)
+        world_report["Enemies"].append(enemy_summary)
+
+    socket.sendall(pickle.dumps(world_report))
+
+def receive_world_report(connection):
+    """receive the pertinent world informtion from the first player"""
+    data = connection.recv(1024)
+    if not data:
+        return None
+    else:
+        return pickle.loads(str(data))
+
+def send_spawn_input(enemy_spawn_summary, socket):
+    """send spawn inputs to the first player"""
+    socket.sendall(pickle.dumps(enemy_spawn_summary))
+
+def receive_spawn_input(connection):
+    """recieve possible spawn inputs from the second player"""
+    data = connection.recv(1024)
+    if not data:
+        return None
+    else:
+        return pickle.loads(str(data))
